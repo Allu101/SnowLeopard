535f6537e01437de7c628444285a2802e95a3d0b
Games4k <hymcgames@gmail.com>
2018-03-21 22:35:25 +0100

    An added an automated ban

--- src/org/encanta/mc/ac/CombatAnalyser.java
+++ src/org/encanta/mc/ac/CombatAnalyser.java
@@ -3,6 +3,8 @@ package org.encanta.mc.ac;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
 
@@ -26,6 +28,11 @@ public class CombatAnalyser {
 		lvq.trainUntil(0.00000000001);
 	}
 
+	private Bukkit getServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public void rebuild(Player callback) throws FileNotFoundException, IOException, InvalidConfigurationException {
 		this.lvq = new LVQNeuronNetwork(0.2, 0.95);
 		for (Dataset dataset : Utils.getAllCategory())
@@ -36,12 +43,18 @@ public class CombatAnalyser {
 				ChatColor.GREEN + "Rebuilded neuron network with epoch(es) " + lvq.trainUntil(0.00000000001));
 	}
 
-	public void sendAnalyse(Player callback, Double[] dump) {
+	public void sendAnalyse(ConsoleCommandSender callback, Double[] dump, Player p) {
 		lvq.printPredictResult(dump);
-		callback.sendMessage(ChatColor.GOLD + "** Analysis Report **");
-		callback.sendMessage(ChatColor.GREEN + "  Best matched: " + ChatColor.YELLOW + lvq.predict(dump).bestMatched);
-		callback.sendMessage(
+		getServer();
+		ConsoleCommandSender console = Bukkit.getConsoleSender();
+		console.sendMessage(ChatColor.GOLD + "** Analysis Report **");
+		console.sendMessage(ChatColor.GREEN + "  Best matched: " + ChatColor.YELLOW + lvq.predict(dump).bestMatched);
+		console.sendMessage(
 				ChatColor.GREEN + "  Euclidean distance: " + ChatColor.YELLOW + lvq.predict(dump).distance);
+		if(lvq.predict(dump).bestMatched.equals("combat.Impossible")){
+			//p.kickPlayer(ChatColor.translateAlternateColorCodes('&', "&c&lUnrealPower&r &8-&r &7Machine Learning (combat.Impossible)"));
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + p.getPlayerListName() + " 7d &c&lUnrealPower&r &8-&r &7Machine Learning (" + lvq.predict(dump).bestMatched + ")");
+		}
 	}
 
 	public void sendInfoToPlayer(Player p) {
